61ac7b9685ff7f6c315a8f723d6018c8043bdc0f
francoisloue <francois.loue@ynov.com>
2023-11-16 14:58:30 +0100

    page de connexion et routeé

--- config/routes/routes.js
+++ config/routes/routes.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('home', {title:"hello"})
 })
 
+router.get('/connection', (req, res) => {
+    res.render('connection', {title: "Connection Page"})
+})
+
 module.exports = router
