d7fff868fb62512640e0e76e4682244d8af9657d
Jagh56 <l.duboisjulienne@gmail.com>
2023-11-16 14:59:46 +0100

    page register

--- config/routes/routes.js
+++ config/routes/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home', {title:"hello"})
+	res.render('home', {title:"Accueil"})
+})
+
+router.get('/register', (req, res) => {
+	res.render('register', {title:"Inscription"})
 })
 
 module.exports = router
