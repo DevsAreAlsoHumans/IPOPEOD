003bdca324c13784687c655b7c5dea8c92674b41
Jagh56 <l.duboisjulienne@gmail.com>
2023-11-16 14:23:49 +0100

    merge dev into ECHO3

--- config/server.js
+++ config/server.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const path = require("path")
+const { engine } = require("express-handlebars")
 const appRouter = require('./routes/routes')
 
 const app = express()
 const port = 8000
 
 function startServer() {
-    app.set('view engine', 'html')
-    app.set('views', path.join(__dirname, "/../App/views"))
-    app.engine('html', require('ejs').renderFile)
-    app.use('/app', appRouter)
+    app.set('view engine', 'handlebars')
+    app.set('views', path.join(__dirname, "../App/views"))
+    app.engine('handlebars', engine())
+    app.use('/App', appRouter)
     app.listen(port, ()=> {
         console.log(`Exemple app listen on http://localhost:${port}/`)
     })
