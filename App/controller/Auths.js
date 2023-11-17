d0f60792679a62fdd6e974ad28d876ef0b56f72e
Jonathan <fatonjonathan@gmail.com>
2023-11-17 11:49:25 +0100

    Auths regist methode create

---
+++ App/controller/Auths.js
@@ -0,0 +1,29 @@
+const compare = require("../utils/compare")
+const compare = require("../library/db-library")
+class Auths {
+    regist(req, res){
+        creatDb();
+        const mdp = compare(res.mdp, res.mdpConfirm)
+        if (!mdp) {
+            res.send("Les mots de passe ne sont pas identiques")
+        }
+        else{
+            const userData = {
+                username: req.body.username, 
+                password: mdp,
+                email: req.body.email,
+            }
+
+            createUser(userData, (err, data) => {
+                if(err) {
+                    console.log("Erreur lors de la création de l'utilisateur", err);
+                    res.status(500).send("Erreur serveur");
+                } else {
+                    console.log("Utilisateur créé avec succès");
+                    res.status(200).send("Utilisateur créé");
+                }
+            }) 
+        
+        }
+    }
+}
--- App/controller/WelcomeController.js
+++ App/controller/WelcomeController.js
@@ -2,10 +2,17 @@
 
 class WelcomeController {
 
-    index(req, res) {
-        res.render('home', {title:"hello"})
+    index(req, res) { 
+        res.render('home', {title:"Hello"})
     }
 
+    register(req, res) {
+        res.render('register', {title:"Inscription"})
+    }
+
+    connection(req, res) {
+        res.render('connection', {title:"Connection"})
+    }
 }
 
 module.exports = WelcomeController;
