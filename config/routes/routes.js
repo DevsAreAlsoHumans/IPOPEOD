c43b61bc3df294b488a966535b10d195811b3ad8
francoisloue <francois.loue@ynov.com>
2023-11-17 11:30:03 +0100

    route working

--- config/routes/routes.js
+++ config/routes/routes.js
@@ -6,10 +6,19 @@ const router = express.Router();
 const WelcomeController = require('../../App/controller/WelcomeController');
 const welcomeController = new WelcomeController();
 
+const AuthController = require('../../App/controller/Auths')
+const authController = new AuthController();
+
 //cette route permet de pointer vers la méthode index du controller welcomeController
 router.get('/', (req, res) => welcomeController.index(req, res));
+
+
 router.get('/register', (req, res) => welcomeController.register(req, res));
+router.post('/register', (req, res) => authController.register(req, res))
+
 router.get('/connection', (req, res) => welcomeController.connection(req, res));
+
+
 
 
 module.exports = router
