305b3d5a3aca817016ec1e8fe4d5d4be4650267b
francoisloue <francois.loue@ynov.com>
2023-11-17 15:11:01 +0100

    framework base

--- config/server.js
+++ config/server.js
@@ -5,14 +5,23 @@ const appRouter = require('./routes')
 const app = express()
 const port = 8000
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-function startServer() {
+async function startServer() {
     app.set('view engine', 'handlebars')
     app.set('views', path.join(__dirname, "../App/views/"))
     app.engine('handlebars', engine())
     app.use('/', appRouter)
+    console.log("Creating server...")
+    await sleep(2000)
+    console.log("Starting server...")
+    await sleep(3500)
+    console.log("Your server is live !")
+    await sleep(1000)
     app.listen(port, ()=> {
-        console.log(`Exemple app listen on http://localhost:${port}/`)
+        console.log(`Exemple app listening on http://localhost:${port}/`)
     })
 }
 
